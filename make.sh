09e4d15478ccf45c7d188fa524726ceb4bea121a
austin <austinpalmore1234@gmail.com>
2018-06-14 23:05:32 -0400

    took the lottory game out still having issues

--- make.sh
+++ make.sh
@@ -2,8 +2,10 @@
 #!/bin/bash
 FNAME=../backup$(date | sed -e 's/ /_/g').zip
 zip -r $FNAME *.sh com/* bin/Manifest.txt bin/com/austinpalmore/fun_with_math/probability/FXMLDocument.fxml *.jar test/*
-javac com/austinpalmore/fun_with_math/Main.java com/austinpalmore/fun_with_math/probability/*.java -d bin/ -Xlint
+#javac com/austinpalmore/fun_with_math/Main.java com/austinpalmore/fun_with_math/probability/*.java -d bin/ -Xlint
+javac com/austinpalmore/fun_with_math/Main.java -d bin/
 cd bin
-jar cfm ../FunWithMath.jar Manifest.txt com/austinpalmore/fun_with_math/* com/austinpalmore/fun_with_math/probability/FXMLDocument.fxml
+#jar cfm ../FunWithMath.jar Manifest.txt com/austinpalmore/fun_with_math/* com/austinpalmore/fun_with_math/probability/FXMLDocument.fxml
+jar cfm ../FunWithMath.jar Manifest.txt com/austinpalmore/fun_with_math/*
 cd ..
 rm -rf primes.txt complex.txt bin/com/austinpalmore/fun_with_math/*.class  bin/com/austinpalmore/fun_with_math/{algorithms,cryptograpy,numbers,probability,util}/*.class
